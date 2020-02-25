3d251ab95fdfa3fcdd51d948e758284a5f8055fd
Ubuntu <stef@proj.xnfsw2nopvje5i3eczbwlx4sna.zx.internal.cloudapp.net>
2020-02-25 19:38:35 +0000

    update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 
-from app import routes
+from app import routes, models
