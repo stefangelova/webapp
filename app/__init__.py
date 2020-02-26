b68940fa35299884d0e27e29af72e62c8938c1f9
stef <s.angelova.14@aberdeen.ac.uk>
2020-02-26 13:29:05 +0000

    trying to display database

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,16 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQL_ALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQL_ALCHEMY_DATABASE_URI'] = "mysql+pymysql://stef@project1db:Thisisstup1d@project1db.mysql.database.azure.com/proj1db"
+db = SQLAlchemy(app)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-from app import routes, models
+class User(db.Model):
+	__tablename__ = 'users'
+	user_id = db.Column('user_id', db.Integer, primary_key=True)
+	username = db.Column('username', db.Unicode, index=True, unique=True)
+	password = db.Column('password', db.Unicode)
+	email = db.Column('email', db.Unicode, index=True, unique=True)
+	book_list_id = db.Column('book_list_id', db.Integer) 
+
+
