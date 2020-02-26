c74de45d00c3fc599d5bc5c3c35479b8e33f86b4
stef <s.angelova.14@aberdeen.ac.uk>
2020-02-26 16:57:10 +0000

    db sort of displayed

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,27 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+ 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQL_ALCHEMY_DATABASE_URI'] = "mysql+pymysql://stef@project1db:Thisisstup1d@project1db.mysql.database.azure.com/proj1db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://stef@project1db:Thisisstup1d@project1db.mysql.database.azure.com/proj1db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class User(db.Model):
-	__tablename__ = 'users'
-	user_id = db.Column('user_id', db.Integer, primary_key=True)
-	username = db.Column('username', db.Unicode, index=True, unique=True)
-	password = db.Column('password', db.Unicode)
-	email = db.Column('email', db.Unicode, index=True, unique=True)
-	book_list_id = db.Column('book_list_id', db.Integer) 
+        __tablename__ = 'users'
+        user_id = db.Column('user_id', db.Integer, primary_key=True)
+        username = db.Column('username', db.Unicode, index=True, unique=True)
+        password = db.Column('password', db.Unicode)
+        email = db.Column('email', db.Unicode, index=True, unique=True)
+        book_list_id = db.Column('book_list_id', db.Integer)
 
+
+class Bookss(db.Model):
+        __tablename__ = 'books'
+        book_id = db.Column('book_id', db.Integer, primary_key=True)
+        title = db.Column('title', db.Unicode)
+        author = db.Column('author', db.Unicode)
+        average_rating = db.Column('average_rating', db.Unicode)
 
